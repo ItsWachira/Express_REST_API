9663c669ba0fc3df546f4b41d4b616daab28af5c
Wachira <80902590+ItsWachira@users.noreply.github.com>
2023-04-12 12:36:27 +0300

    Update db.js

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const knex = require('knex');
 const db = knex({
   client: 'pg',
   connection: {
-    connectionString: 'postgres://express_rest_api_db_user:zRgyR190ullwbWtmJvwq8zwdg5lue1WY@dpg-cgqjl2m4dad5es18mrk0-a/express_rest_api_db',
+    connectionString: ' internal/external database URL',
     ssl: {
       rejectUnauthorized: false
     }
